7f17410f6d39c3f690d9118b6c350c4d85818d0d
Novabot <novabot503@gmail.com>
2026-03-06 11:33:45 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,9 @@
+module.exports = {
+    TELEGRAM_TOKEN: "8332999387:AAEoKIG7UlB621b6QG7djNeIK69BJSXq45I",
+    OWNER_ID: "7587303225",
+    PORT: 8080,
+    HOST: 'localhost',
+    DEVELOPER: '@Novabot403',
+    VERSI_WEB: '1.0',
+    URL: 'https://api.nova.vercel.app'
+};
